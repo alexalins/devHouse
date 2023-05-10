3dc625c1364e88572dba63234e75a57bfd3de370
Alexa Lins <alexa.lins@capgemini.com>
2023-05-10 17:40:38 -0300

    add validação se user ja existe

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,7 +3,12 @@ import User from '../models/User';
 class SessionController {
     async store(req, res) {
         const { email } = req.body;
-        let user = await User.create({ email });
+        let user = await User.findOne({ email });
+
+        if(!user) {
+            user = await User.create({ email });
+        }
+        
         return res.json(user);
     }
 }
