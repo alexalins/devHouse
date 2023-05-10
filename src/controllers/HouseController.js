c7614d55a3a334f9616c23d76b87708b02760b54
Alexa Lins <alexa.lins@capgemini.com>
2023-05-10 19:09:44 -0300

    criando update de casa

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,12 +1,18 @@
 import House from '../models/House';
 
 class HouseController {
+    async index(req, res) {
+        const { status } = req.query;
+        const houses = await House.find({ status });
+        return res.json(houses);
+    }
+
     async store(req, res) {
         const { filename } = req.file;
         const { description, price, location, status } = req.body;
         const { user_id } = req.headers;
 
-        const house = await House.create({
+        await House.create({
             user: user_id,
             thumbnail: filename,
             description,
@@ -15,7 +21,25 @@ class HouseController {
             status
         });
 
-        return res.json(house);
+        return res.json({status: 200});
+    }
+
+    async update(req, res) {
+        const { house_id } = req.params;
+        const { description, price, location, status } = req.body;
+        const { user_id } = req.headers;
+        const { filename } = req.file ? req.file : '';
+
+        await House.updateOne({ _id: house_id }, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status
+        });
+
+        return res.json({status: 200});
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,13 @@ import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 
-
 const routes = new Router();
-const uploads = multer(uploadConfig);
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
-routes.post('/houses', uploads.single('thumbnail'), HouseController.store);
 
+routes.get('/houses', HouseController.index);
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 
 export default routes;
